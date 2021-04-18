0f1482b5adb644b6f95b2a3b106eb093bbee0aaa
Agus <>
2021-04-18 20:17:33 +0000

    Added another conditional based on age so that range 45 to 55 years dont pay ticket

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
-print("Welcome to the Rollercoaster Ride. You will be charged after your answers.")
+print("Welcome to the Rollercoaster Ride. You will be charged accordingly:")
 height = int(input("Please enter your height in cm without decimals: "))
 age = int(input("What is your age?: "))
-photo = input("Do you want your picture taken? (Y / N): ")
 bill = 0
 
 #For all heigher than 120 cm
 if height >= 120:
-  print("You can ride.")
   if age < 12:
     bill = 5
-    print("Please pay $5.")
   elif age <= 18:
     bill = 7
-    print("Please pay $7.")
+  elif age >= 45 and age <= 55:
+    bill = 0
   else:
     bill = 12
-    print("Please pay $12.")
-  #Add $3 to bill      
+
+  #Add $3 to bill
+  photo = input("Do you want your picture taken? (Y / N): ")    
   if photo == "Y":
     bill += 3
-
 #Happens if Y and if N so respects indentation
   print(f"Your final bill is $ {bill}.")
 #For those lower than 120 cm    
